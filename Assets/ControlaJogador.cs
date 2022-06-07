b8f2640f00197a0318959db175e68d690cc12242
Jeniffer Macena de Souza <61254473+jenmacena19@users.noreply.github.com>
2022-06-07 01:33:25 -0400

    Initial commit

---
+++ Assets/ControlaJogador.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlaJogador : MonoBehaviour
+{
+    public float velocidade;
+    // Update is called once per frame
+    void Update()
+    {
+        float eixoX = Input.GetAxis("Horizontal");
+        float eixoZ = Input.GetAxis("Vertical");
+
+        Vector3 direcao = new Vector3(eixoX, 0, eixoZ);
+
+        transform.Translate(direcao * velocidade * Time.deltaTime);
+        if (direcao != Vector3.zero)
+        {
+            GetComponent<Animator>().SetBool("Movendo", true);
+        }
+        else
+        {
+            GetComponent<Animator>().SetBool("Movendo", false);
+        }
+    }
+}
